ab938f1639604bf5d9d098ab020c2e6cebf3a6e3
Vishal Jain <vishaljain_987@yahoo.com>
2020-07-11 08:34:37 +0530

    commit

---
+++ src/test/java/io/bethebest/pmtool/PmtoolApplicationTests.java
@@ -0,0 +1,13 @@
+package io.bethebest.pmtool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PmtoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
