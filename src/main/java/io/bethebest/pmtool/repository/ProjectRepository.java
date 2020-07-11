4cd45da778756fc9e685d355d5c38d846c1bb574
Vishal Jain <vishaljain_987@yahoo.com>
2020-07-11 21:50:28 +0530

    Project model and repository

---
+++ src/main/java/io/bethebest/pmtool/repository/ProjectRepository.java
@@ -0,0 +1,13 @@
+package io.bethebest.pmtool.repository;
+
+import java.util.Optional;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import io.bethebest.pmtool.model.Project;
+
+@Repository
+public interface ProjectRepository extends CrudRepository<Project, Long>{
+	
+}
