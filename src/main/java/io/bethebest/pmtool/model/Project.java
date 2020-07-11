7bab96fddbd0b4ce131bb6c691ddb681cb578a8e
Vishal Jain <vishaljain_987@yahoo.com>
2020-07-11 23:06:46 +0530

    project validation part 1

--- src/main/java/io/bethebest/pmtool/model/Project.java
+++ src/main/java/io/bethebest/pmtool/model/Project.java
@@ -2,12 +2,18 @@ package io.bethebest.pmtool.model;
 
 import java.util.Date;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
+import javax.persistence.UniqueConstraint;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 @Entity
 public class Project {
@@ -16,12 +22,27 @@ public class Project {
 	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Long id;
 	
+	@NotBlank(message="Project Name is required")
 	private String projectName;
+	
+	@NotBlank(message="Project Identifier is required")
+	@Size(max=6, min=4, message="Please use 4 to 5 characters")
+	@Column(updatable=false, unique=true)
 	private String projectIdentifier;
+	
+	@NotBlank(message="Project Description is required")
 	private String description;
+	
+	@JsonFormat(pattern="yyyy-mm-dd")
 	private Date start_date;
+	
+	@JsonFormat(pattern="yyyy-mm-dd")
 	private Date end_date;
+	
+	@JsonFormat(pattern="yyyy-mm-dd")
 	private Date created_At;
+	
+	@JsonFormat(pattern="yyyy-mm-dd")
 	private Date updated_At;
 	
 	public Project(){
