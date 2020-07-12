58a0fe29581d0c4f59fb76dbbcc2122f764a0fa8
Vishal Jain <vishaljain_987@yahoo.com>
2020-07-13 01:32:56 +0530

    update project

--- src/main/java/io/bethebest/pmtool/controller/ProjectController.java
+++ src/main/java/io/bethebest/pmtool/controller/ProjectController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -64,6 +65,16 @@ public class ProjectController {
 		
 		projectService.deleteByProjectIdentifier(projectIdentifier);
 		return new ResponseEntity<String>("Project with ID: "+projectIdentifier+" deleted successfully", HttpStatus.OK);
+		
+	}
+	
+	@PutMapping("")
+	public ResponseEntity<?> updateProject(@Valid @RequestBody Project project, BindingResult result){
+		ResponseEntity errorMap = validationErrorMapService.validationErrorMap(result);
+		if(errorMap!=null) return errorMap;
+		
+		Project newProject = projectService.saveOrUpdate(project);
+		return new ResponseEntity<Project>(newProject, HttpStatus.OK);
 		
 	}
 }
