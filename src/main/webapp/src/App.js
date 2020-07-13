ae17b267e58009164a027a69e415871cdffe6f4f
Vishal Jain <vishaljain_987@yahoo.com>
2020-07-14 01:19:31 +0530

    added react router dom

--- src/main/webapp/src/App.js
+++ src/main/webapp/src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Dashboard from './components/Dashboard';
 import Header from './components/Layout/Header';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import AddProject from './components/Project/AddProject';
 
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Dashboard />
+      <Router>
+        <Header />
+        <Route exact path="/dashboard" component={Dashboard} />
+        <Route exact path="/addProject" component={AddProject} />
+      </Router>
     </div>
   );
 }
